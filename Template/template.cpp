24763ebc524eceefe0eeb804b95081af721cbd97
Akash <akashvana2001@gmail.com>
2020-05-25 07:55:17 +0530

    updated the template

--- Template/template.cpp
+++ Template/template.cpp
@@ -1,9 +1,9 @@
 /******* all required header files *******/
-#include<iostream>
-#include<algorithm>
-#include<iomanip>
-#include<cmath>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
 #include <string>
+#include <iomanip>
 #include <vector>
 #include <algorithm>
 #include <sstream>
@@ -31,24 +31,24 @@ using namespace std;
 #define w(t) int t; cin>>t; while(t--)
 #define vi vector<int>
 #define vs vector<string>
+#define vc vector<char>
 #define mp make_pair
 #define ll long long int
+#define pb push_back
+#define F first
+#define S second
 /***************************/
 
 int main()
 {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
 
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-
-
-
-
-    return 0;
+	return 0;
 }
