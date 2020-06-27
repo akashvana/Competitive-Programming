8f486fd894f56ff36a245bcdbcff4b89ef1e80c7
Akash <akashvana2001@gmail.com>
2020-06-27 22:20:44 +0530

    not complete

---
+++ Codeforces/Practice/Div . 2 A/799A-Carrot-cakes.cpp
@@ -0,0 +1,66 @@
+/******* all required header files *******/
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+
+/*****All definitions*******/
+#define w(t) int t; cin>>t; while(t--)
+#define vi vector<int>
+#define vs vector<string>
+#define vc vector<char>
+#define mp make_pair
+#define ll long long int
+#define pb push_back
+#define F first
+#define S second
+/***************************/
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	double n, t, k, d;
+	cin>>n>>t>>k>>d;
+
+	if(k >= n)
+	{
+		cout<<"NO";
+		return 0;
+	}
+
+	if((t * ceil(n/k)) <= (d + t*((n/2)/k)))
+		cout<<"NO";
+	else cout<<"YES";
+
+	return 0;
+}
