685c7f93150170710960329c113fce672e1d0ed0
Akash <akashvana2001@gmail.com>
2020-07-02 21:03:29 +0530

    complete

--- Atcoder/Contests/ABC167/D.cpp
+++ Atcoder/Contests/ABC167/D.cpp
@@ -36,10 +36,10 @@ using namespace std;
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// #endif
 
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
