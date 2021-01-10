15e25d8cdbfb291116851801208db94b51ce775d
Akash <akashvana2001@gmail.com>
2021-01-10 19:52:35 +0530

    solved

---
+++ Atcoder/Contests/ABC188/A.cpp
@@ -0,0 +1,66 @@
+/******* all required header files *******/
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+
+/*****All definitions*******/
+#define w(t) int t; cin>>t; while(t--)
+#define vi vector<int>
+#define vs vector<string>
+#define vc vector<char>
+#define mp make_pair
+#define ll long long int
+#define pb push_back
+#define F first
+#define S second
+/***************************/
+
+int main()
+{
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// #endif
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	
+
+		ll a, b;
+		cin>>a>>b;
+		if((a-b < 3 && a-b >= 0) || (b-a < 3 && b-a >= 0)){
+			cout<<"Yes"<<endl;
+		}else{
+			cout<<"No"<<endl;
+		}
+
+
+	
+
+	return 0;
+}
